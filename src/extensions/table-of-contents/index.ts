8e0d943b5f077b716c9f0053a80297fb96dc0327
Aditya <aditya@example.com>
2026-06-12 13:09:42 +0530

    feat: Add index.ts

---
+++ src/extensions/table-of-contents/index.ts
@@ -0,0 +1,25 @@
+import { Node, mergeAttributes } from '@tiptap/core';
+import { ReactNodeViewRenderer } from '@tiptap/react';
+import { TOCNodeView } from './TOCNodeView';
+
+export const TableOfContentsExtension = Node.create({
+  name: 'tableOfContents',
+  group: 'block',
+  selectable: true,
+  draggable: true,
+  atom: true,
+
+  parseHTML() {
+    return [
+      { tag: 'div[data-type="table-of-contents"]' }
+    ];
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ['div', mergeAttributes(HTMLAttributes, { 'data-type': 'table-of-contents' })];
+  },
+
+  addNodeView() {
+    return ReactNodeViewRenderer(TOCNodeView);
+  },
+});
